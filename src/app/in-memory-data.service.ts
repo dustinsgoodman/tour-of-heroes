83b4f71ebea615bd3b701f300293208e10a94f89
Dustin Goodman <dustin.s.goodman@gmail.com>
2016-11-25 21:58:54 -0600

    update hero class to be more complex

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -1,18 +1,17 @@
 import { InMemoryDbService  } from 'angular-in-memory-web-api';
+import { Hero } from './hero';
 
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
-    let heroes = [
-      { id: 11, name: 'Mr. Nice' },
-      { id: 12, name: 'Narco' },
-      { id: 13, name: 'Bombasto' },
-      { id: 14, name: 'Celeritas' },
-      { id: 15, name: 'Magneta' },
-      { id: 16, name: 'RubberMan' },
-      { id: 17, name: 'Dynama' },
-      { id: 18, name: 'Dr IQ' },
-      { id: 19, name: 'Magma' },
-      { id: 20, name: 'Tornado' }
+    let heroes: Hero[] = [
+      new Hero(11, 'Wolverine', 'Accelerated healing & bone claws', 'Logan'),
+      new Hero(12, 'Cyclops', 'Laser eyes', 'Scott Summers'),
+      new Hero(13, 'Superman', 'Super strength, speed, hearing, breath, invincibility, etc.', 'Clark Kent'),
+      new Hero(14, 'Batman', 'World\'s greatest detective', 'Bruce Wayne'),
+      new Hero(15, 'Iron Man', 'Rich kid with toys', 'Tony Stark'),
+      new Hero(16, 'Hulk', 'Super strength and regenerative healing induced by rage', 'Bruce Banner'),
+      new Hero(17, 'Martian Man Hunter', 'Telepathy, shape shifter, phasing, strength, flight', 'John Jones'),
+      new Hero(18, 'The Flash', 'Super speed', 'Barry Allen')
     ];
     return {heroes};
   }
